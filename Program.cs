baa38ce72de99dd80eba4116a8c8c9b2a05fbd82
DESKTOP-7R72E4P\Lenovo <NIKINISANTHAN@GMAIL.COM>
2024-07-05 17:41:32 +0530

    CHECK THE THREAD WORKING

--- Program.cs
+++ Program.cs
@@ -10,12 +10,18 @@ namespace API_BAS
     {
         static async Task Main(string[] args)
         {
+            int workerThreads = 4;
+            int completionPortThreads = 4;
+
+            ThreadPool.SetMinThreads(workerThreads, completionPortThreads);
+            ThreadPool.SetMaxThreads(workerThreads, completionPortThreads);
             using (HttpClient client = new HttpClient())
             {
+
                 var requestBody = new Dictionary<string, string>
             {
-                { "username", "6dce2f9ece1e45d1aa0d3507f2eabc86" },
-                { "password", "8b7f51d2aef34eccafb7ac00363556f6" },
+                { "username", "ec32110b5a8f46ff8596ac6e12042539" },
+                { "password", "c327d2225298409da288af55744672ae" },
                 { "grant_type", "password" }
             };
 
@@ -40,7 +46,7 @@ namespace API_BAS
 
                 // Add headers to the request message
                 // request.Headers.Add("Authorization", ""BASIC", "MzYzMzE3OTJmMDcwNDczNDlhYmMzZTYwNmQ0ZTY4ODk6Y2NkODMxM2U4MWE0NGVhMzkxODFmMjk3NGE1OTU1M2E="");
-                request.Headers.Authorization = new AuthenticationHeaderValue("BASIC", "NmRjZTJmOWVjZTFlNDVkMWFhMGQzNTA3ZjJlYWJjODY6OGI3ZjUxZDJhZWYzNGVjY2FmYjdhYzAwMzYzNTU2ZjY=");
+                request.Headers.Authorization = new AuthenticationHeaderValue("BASIC", "ZWMzMjExMGI1YThmNDZmZjg1OTZhYzZlMTIwNDI1Mzk6YzMyN2QyMjI1Mjk4NDA5ZGEyODhhZjU1NzQ0NjcyYWU=");
                
                 try
                 {
@@ -54,7 +60,7 @@ namespace API_BAS
                   //  Console.WriteLine(TokenResponse);
                     var tokenData = Newtonsoft.Json.JsonConvert.DeserializeObject<TokenResponse>(TokenResponse);
                   //  Console.WriteLine(tokenData.access_token);
-                    await api_search(tokenData.access_token);
+                    await api_search(tokenData.access_token,0,0);
                 }
                 catch (HttpRequestException e)
                 {
@@ -64,47 +70,78 @@ namespace API_BAS
             
             
     }
-        public static async Task api_search(string access_token)
+        public static async Task api_search(string access_token,int count,int try_count)
         {
-            using (HttpClient client = new HttpClient())
+            Thread thread = new Thread(new ThreadStart(DoWork));
+            thread.Start();
+            thread.Join();
+            using (StreamWriter writer = new StreamWriter("E:\\API\\LOG.txt"))
             {
-                Console.WriteLine("");
-                Console.WriteLine("START TIME   :" + DateTime.Now);
+                using (HttpClient client = new HttpClient())
+             {
+                    writer.WriteLine(" ");
+                    writer.WriteLine("START TIME   :" + DateTime.Now);
+                    for(int j=0; j<=23; j++)
+                    {
+                        
+                        int e_TIME = j + 1;
+                        
 
-                for (int i = 0; i < 5500; i=i+50)
+                        for (int i = count; i < 5500; i=i+50)
                 {
 
                     string jsonBody_1 = @"
                 {
                     ""start_index"":";
-                    string Count_st = Convert.ToString(i);
+                            string Count_st = Convert.ToString(i);
 
-                    string jsonBody_2 = @",""max_count"": 50,
-                    ""client_code"": ""URBANRISE_LIFESTYLES"",
+                            string jsonBody_2 = @",""max_count"": 50,
+                    ""client_code"": ""LEIGHTON"",
                     ""site_code"": ""ALL"",
                     ""location_code"": ""ALL"",
                     ""mode"": ""D"",
-                    ""fromdate"": ""2024-06-01T19:20:30.45+01:00"",
-                    ""todate"": ""2024-06-15T19:20:30.45+01:00""
+                    ""fromdate"": ""2024-04-04T";
+                            string s_date;
+                            if (j < 10)
+                            {
+                                s_date = "0" + Convert.ToString(j);
+                            }
+                            else
+                            {
+                                s_date = Convert.ToString(j);
+                            }
+                            string jsonBody_3 = @":00:00.00+01:00"",
+                    ""todate"": ""2024-04-04T";
+                            string E_date ;
+                            if (j < 10)
+                            {
+                                 E_date = "0"+Convert.ToString(e_TIME);
+                            }
+                            else
+                            {
+                                E_date = Convert.ToString(e_TIME);
+                            }
+                            string jsonBody_4 = @":59:59.00+01:00""
                 }";
-                    string jsonBody = jsonBody_1 + Count_st + jsonBody_2;
+                            string jsonBody = jsonBody_1 + Count_st + jsonBody_2+s_date+ jsonBody_3+ E_date + jsonBody_4;
                     Console.WriteLine(jsonBody);
-                    /* 
-                       string jsonBody = @"
-                       {
-                           ""start_index"": 1,
-                           ""max_count"": 50,
-                           ""client_code"": ""URBANRISE_LIFESTYLES"",
-                           ""site_code"": ""ALL"",
-                           ""location_code"": ""ALL"",
-                           ""mode"": ""D"",
-                           ""fromdate"": ""2024-06-01T19:20:30.45+01:00"",
-                           ""todate"": ""2024-06-05T19:20:30.45+01:00""
-                       }";
-                      */
+                        writer.WriteLine(jsonBody);
+                        /* 
+                           string jsonBody = @"
+                           {
+                               ""start_index"": 1,
+                               ""max_count"": 50,
+                               ""client_code"": ""URBANRISE_LIFESTYLES"",
+                               ""site_code"": ""ALL"",
+                               ""location_code"": ""ALL"",
+                               ""mode"": ""D"",
+                               ""fromdate"": ""2024-06-01T19:20:30.45+01:00"",
+                               ""todate"": ""2024-06-05T19:20:30.45+01:00""
+                           }";
+                          */
 
-                    // Create the content for the request
-                    StringContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+                        // Create the content for the request
+                        StringContent content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
 
                     // Create the request message
                     HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://mybas.cloud/prod/api/vendor/SearchTransaction")
@@ -115,26 +152,65 @@ namespace API_BAS
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", access_token);
 
 
-                    try
-                    {
-                        // Make the GET request
-                        HttpResponseMessage response = await client.SendAsync(request);
+                            try
+                            {
+                                // Make the GET request
+                                HttpResponseMessage response = await client.SendAsync(request);
 
-                        // Ensure the request was successful
-                        // response.EnsureSuccessStatusCode();
+                                // Ensure the request was successful
+                                // response.EnsureSuccessStatusCode();
 
-                        // Read the response content as a string
-                        string responseBody = await response.Content.ReadAsStringAsync();
-                        Console.WriteLine(responseBody);
-                    }
-                    catch (HttpRequestException e)
-                    {
-                        Console.WriteLine("Request error: " + e.Message);
-                    }
-                    Console.WriteLine("");
+                                // Read the response content as a string
+                                string responseBody = await response.Content.ReadAsStringAsync();
+                                Console.WriteLine(responseBody);
+
+                                // Write the content to the file
+                                if (responseBody == "[]")
+                                {
+                                    i = 5500;
+                                }
+
+                                writer.WriteLine(responseBody);
+
+                            }
+
+                            catch (Exception e)
+                            {
+                                writer.WriteLine("Request error: " + e.Message + " Try again " + try_count + " start_index " + count);
+
+                                Console.WriteLine("Request error: " + e.Message + " Try again " + try_count + " start_index " + count);
+                                if (try_count >= 3)
+                                {
+
+                                    writer.WriteLine(" ");
+                                    writer.WriteLine("END TIME   :" + DateTime.Now);
+                                    break;
+                                }
+                                else
+                                {
+                                    writer.WriteLine(" ");
+                                    writer.WriteLine("END TIME   :" + DateTime.Now);
+                                    await api_search(access_token, count, try_count);
+                                }
+                            }
+                        }
+                        
+
+                        Console.WriteLine("Thread has completed.");
+                        Console.WriteLine("");
                     Console.WriteLine("END TIME   :"+DateTime.Now);
-                }
+                    writer.WriteLine(" ");
+                    writer.WriteLine("END TIME   :" + DateTime.Now);
+                    }
+                   
+               }
 
+            }
+            static void DoWork()
+            {
+                // Simulate work
+                Thread.Sleep(5000);
+                Console.WriteLine("Work completed.");
             }
         }
         public class TokenResponse
